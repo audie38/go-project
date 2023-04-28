eab071d6f7844cba28e69e1de4dacffe32e9e4b6
Audie Milson <milson.audie@gmail.com>
2023-04-28 14:03:41 +0700

    Go DataTypes

---
+++ golang-dasar/datatype.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Numbers
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga koma Lima = ", 3.5)
+
+	// Boolean
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+
+	// String
+	fmt.Println("Audie Milson")
+	fmt.Println(len("Audie Milson"))
+	fmt.Println("Audie Milson"[0])
+}
