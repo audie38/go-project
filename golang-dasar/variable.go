1daff02fe4e6fda83049ed402f0ea3131e82c4e1
Audie Milson <milson.audie@gmail.com>
2023-04-28 14:15:34 +0700

    Go Variables

---
+++ golang-dasar/variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Audie"
+	fmt.Println(name)
+
+	var age = 23
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Audie"
+		lastName = "Milson"
+	)
+	fmt.Println(firstName, lastName)
+}
