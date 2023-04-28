2d3dd110100ad7fc950c68409f453d02b79cda4c
Audie Milson <milson.audie@gmail.com>
2023-04-28 14:31:50 +0700

    Go Operation

---
+++ golang-dasar/operation.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	var(
+		a = 10
+		b = 10
+	)
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+}
