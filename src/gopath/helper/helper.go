eb962fb47250ebc7614175c19831002678e36cc1
Audie Milson <milson.audie@gmail.com>
2023-04-29 21:19:18 +0700

    Go Access Modifier

--- src/gopath/helper/helper.go
+++ src/gopath/helper/helper.go
@@ -2,6 +2,15 @@ package helper
 
 import "fmt"
 
+/**
+PascalCase -> Public Access Modifier, can be access from external
+camelCase -> Private Access Modifier, cannot be accessed from external
+*/
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func sayGoodBye(name string){
+	fmt.Println("Goodbye", name)
 }
--- src/gopath/import.go
+++ src/gopath/import.go
@@ -4,4 +4,5 @@ import "gopath/helper"
 
 func main() {
 	helper.SayHello("Audie")
+	// helper.sayGoodBye("Audie") // Error because private access modifier
 }
