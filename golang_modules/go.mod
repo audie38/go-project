9e58552a377819af16d4b947e7c6a638a90ad32c
Audie Milson <milson.audie@gmail.com>
2023-04-30 00:34:48 +0700

    Go Modules

---
+++ golang_modules/go.mod
@@ -0,0 +1,5 @@
+module github.com/audie38/go-project
+
+go 1.20
+
+require github.com/audie38/go-module-sample v1.0.0 // indirect
---
+++ golang_modules/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/audie38/go-module-sample"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
