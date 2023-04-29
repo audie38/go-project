98c9decb24c0079ac557a0580255269cb3e65faa
Audie Milson <milson.audie@gmail.com>
2023-04-29 14:47:36 +0700

    Go Struct

---
+++ golang-dasar/struct.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Customer struct {
+	Name, Address string
+	Age           int
+}
+
+func main() {
+	var cust Customer
+	cust.Name = "Audie"
+	cust.Address = "Jalan Test"
+	cust.Age = 23
+
+	fmt.Println(cust)
+
+	cust1 := Customer{
+		Name: "Milson",
+		Address: "Jalan Test 1",
+		Age: 23,
+	}
+
+	fmt.Println(cust1)
+}
