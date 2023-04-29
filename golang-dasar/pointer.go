d41ac2d1f6745d317f6c3dc7644d4f4b4399c011
Audie Milson <milson.audie@gmail.com>
2023-04-29 20:52:30 +0700

    Go Pointer (Function)

--- golang-dasar/pointer.go
+++ golang-dasar/pointer.go
@@ -3,25 +3,33 @@ package main
 import "fmt"
 
 type Address struct {
-	city, province, country string
+	City, Province, Country string
+}
+
+func ChangeAddressToIndonesia(address Address){
+	address.Country = "Indonesia"
+}
+
+func ChangeAddrToIndonesia(address *Address){
+	address.Country = "Indonesia"
 }
 
 func main() {
 	addr1 := Address{
-		city:     "Subang",
-		province: "Jawa Barat",
-		country:  "Indonesia",
+		City:     "Subang",
+		Province: "Jawa Barat",
+		Country:  "Indonesia",
 	}
 
 	// pointer reference to addr1 
 	addr2 := &addr1 // use pointer (& symbol) to change go default pass by value to pass by reference
-	addr2.city = "Jakarta"
+	addr2.City = "Jakarta"
 
 	// if reassign, addr1 will not change (pointer reference to struct address)
 	addr2 = &Address{
-		city: "Jakarta",
-		province: "DKI Jakarta",
-		country: "Indonesia",
+		City: "Jakarta",
+		Province: "DKI Jakarta",
+		Country: "Indonesia",
 	}
 
 	fmt.Println("Address1 not change")
@@ -32,9 +40,9 @@ func main() {
 
 	// use the *operator to make the address 1 changes as well (pointer reference to the whole addr3 instance)
 	*addr3 = Address{
-		city: "Malang",
-		province: "Jawa Timur",
-		country: "Indonesia",
+		City: "Malang",
+		Province: "Jawa Timur",
+		Country: "Indonesia",
 	}
 	
 	fmt.Println("Address1 change")
@@ -43,6 +51,10 @@ func main() {
 
 	// create new pointer instance
 	var addr4 *Address = new(Address)
-	addr4.city = "Singapore"
+	addr4.City = "Singapore"
+	ChangeAddressToIndonesia(*addr4) // data will not changed
 	fmt.Println(addr4)
+	ChangeAddrToIndonesia(addr4) // data will change
+	fmt.Println(addr4)
+
 }
