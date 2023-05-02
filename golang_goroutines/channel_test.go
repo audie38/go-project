1a6208220792ad370d98b8dbfa00992dc322d9f0
Audie Milson <milson.audie@gmail.com>
2023-05-02 20:57:07 +0700

    Go Channel as Parameter, Input/Ouput

--- golang_goroutines/channel_test.go
+++ golang_goroutines/channel_test.go
@@ -9,6 +9,9 @@ import (
 /**
 - Channel used to send/received data in go routines
 - Channel must have send and receive, if not will trigger error / deadlock
+- Channel by default pass by reference
+- Channel <- : send
+- <-Channel  : receive
 */
 
 func TestCreateChannel(t *testing.T) {
@@ -22,5 +25,41 @@ func TestCreateChannel(t *testing.T) {
 	}()
 
 	fmt.Println(<- channel) //Receive
+	time.Sleep(5 * time.Second)
+}
+
+// Channel as parameter
+func GiveMeResponse(channel chan string){
+	time.Sleep(2 * time.Second)
+	channel <- "Audie"
+}
+
+func TestChannelAsParameter(t *testing.T){
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <- channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string){
+	time.Sleep(2 * time.Second)
+	channel <- "Audie"
+}
+
+func OnlyOut(channel <-chan string){
+	data := <- channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T){
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
 	time.Sleep(5 * time.Second)
 }
