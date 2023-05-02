ab82d3cbfdd72ded60cec2fe7d30bd9e18f07e46
Audie Milson <milson.audie@gmail.com>
2023-05-02 23:01:07 +0700

    Go Sync.Map

---
+++ golang_goroutines/map_test.go
@@ -0,0 +1,34 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func AddToMap(data *sync.Map, value int, group *sync.WaitGroup){
+	defer group.Done()
+	group.Add(1)
+
+	data.Store(value, value)
+	time.Sleep(1 * time.Second)
+}
+
+func TestMap(t *testing.T){
+	data := &sync.Map{}
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go AddToMap(data, i, group)
+	}
+
+	group.Wait()
+
+	data.Range(func(key, value interface{}) bool{
+		fmt.Println(key, ":", value)
+		return true
+	})
+
+	fmt.Println("Done")
+}
