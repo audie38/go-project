e6420670db372ff07747568fa2ca1ee06a7a4741
Audie Milson <milson.audie@gmail.com>
2023-05-02 21:44:33 +0700

    Go RWMutex

--- golang_goroutines/mutex_test.go
+++ golang_goroutines/mutex_test.go
@@ -28,3 +28,37 @@ func TestMutex(t *testing.T){
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter = ", x)
 }
+
+type BankAccount struct{
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (acc *BankAccount) AddBalance(amount int){
+	acc.RWMutex.Lock()
+	acc.Balance += amount
+	acc.RWMutex.Unlock()
+}
+
+func (acc *BankAccount) GetBalancce() int{
+	acc.RWMutex.RLock()
+	balance := acc.Balance
+	acc.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T){
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func(){
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalancce())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalancce())
+}
