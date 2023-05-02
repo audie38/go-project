e24ec1e4ce64110464f07609ff8a4a2e7963048d
Audie Milson <milson.audie@gmail.com>
2023-05-02 21:36:29 +0700

    Go Mutex

---
+++ golang_goroutines/mutex_test.go
@@ -0,0 +1,30 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+/**
+Mutex : prevent race condtion by locking the shared variables/data
+*/
+
+func TestMutex(t *testing.T){
+	x := 0
+	var mutex sync.Mutex
+
+	for i := 1; i <= 1000; i++{
+		go func(){
+			for j:= 1; j<= 100; j++{
+				mutex.Lock()
+				x += 1
+				mutex.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}
